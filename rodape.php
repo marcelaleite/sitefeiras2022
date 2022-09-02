025fd2bd508034ca93c9414c06eb38003539eb7c
pedro hdefler <pedrohdeifler555@gmail.com>
2022-09-02 01:32:25 -0300

    modified:   index.php 	modified:   menu.php 	new file:   rodape.php 	deleted:    teste

---
+++ rodape.php
@@ -0,0 +1,25 @@
+<div class="card" style="Background-color:#5dc460">
+  <div class="card-header">
+    Sobre:
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="row">
+        <div class="col"><h5 class="card-title">Apoio</h5></div>
+        <div class="col"><h5>Feito Por</h5></div>    
+        </div>
+        <div class="col">
+            <img src="imagens/UFSC Blumenau.png">
+        </div>
+        <div class="col">
+            <img src="imagens/FURB.png">
+        </div>
+        <div class="col">
+            <img src="imagens/IFC_vertical.png">
+        </div>
+        <div class="col">
+            <img src="imagens/SBEM.png" alt="">
+        </div>
+        </div>
+    </div>
+</div>
